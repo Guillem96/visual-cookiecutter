1726c44934f4f9d17830a4511ee04e80069a213d
Guillem Orellana Trullols <guillem.orellana@gmail.com>
2023-04-18 15:35:04 +0200

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-from viz_cookiecutter.core import VisualCookiecutterContext, VisualCookiecutter
-
-ctx = VisualCookiecutterContext.parse_obj({})
-VisualCookiecutter(cookiecutter_params=[
-    1,
-], context=ctx)
